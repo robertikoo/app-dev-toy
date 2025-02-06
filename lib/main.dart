46edc62fd8fbf81a859a75962e0546facc13791c
mpsdev <mparedes.digital@gmail.com>
2025-02-06 20:18:58 -0300

    feat: add providers and nav routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
-import 'screens/welcome_screen.dart';
+import 'package:provider/provider.dart';
+import 'features/welcome/screens/welcome_screen.dart';
+import 'features/chat/providers/chat_thread_provider.dart';
+import 'features/chat/providers/chat_provider.dart';
+import 'features/chat/services/chat_service.dart';
+import 'features/home/screens/home_screen.dart';
+import 'features/chat/screens/chattray_screen.dart';
+import 'features/profile/screens/profile_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,8 +17,32 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: WelcomeScreen(),
+    return MultiProvider(
+      providers: [
+        Provider<ChatService>(
+          create: (_) => ChatServiceImpl(),
+        ),
+        ChangeNotifierProxyProvider<ChatService, ChatProvider>(
+          create: (context) => ChatProvider(context.read<ChatService>()),
+          update: (context, chatService, previous) => 
+            previous ?? ChatProvider(chatService),
+        ),
+        ChangeNotifierProxyProvider<ChatService, ChatThreadProvider>(
+          create: (context) => ChatThreadProvider(context.read<ChatService>()),
+          update: (context, chatService, previous) => 
+            previous ?? ChatThreadProvider(chatService),
+        ),
+      ],
+      child: MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const WelcomeScreen(),
+          '/home': (context) => const HomeScreen(),
+          '/chat': (context) => const ChatTrayScreen(),
+          '/profile': (context) => const ProfileScreen(),
+        },
+      ),
     );
+
   }
 }
