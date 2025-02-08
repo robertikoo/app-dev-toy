3dc47ab3983fccef13df543781befeb7e33db818
robertikoo <45905013+robertikoo@users.noreply.github.com>
2025-02-07 22:28:48 -0300

    fix: Home-List Update Search Card

--- lib/features/home/widgets/search_card.dart
+++ lib/features/home/widgets/search_card.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
 
 class SearchCard extends StatelessWidget {
-  const SearchCard({super.key});
+  final ValueChanged<String> onSearchChanged; // Llamado cuando el texto cambia
+
+  const SearchCard({super.key, required this.onSearchChanged});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: 'Buscar...',
-              prefixIcon: const Icon(Icons.search),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              '',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            TextField(
+              onChanged: onSearchChanged, // Se llama a onSearchChanged cuando cambia el texto
+              decoration: const InputDecoration(
+                labelText: 'Buscar...',
+                prefixIcon: const Icon(Icons.search), // Icono de lupa
+                border: OutlineInputBorder(),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
   }
-} 
+}
