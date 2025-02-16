434f9090e051549ffc54eff42482236eca29f004
robertikoo <45905013+robertikoo@users.noreply.github.com>
2025-02-16 04:27:23 -0300

    fix: Cuidado Esporadico update lib/main.dart add idiome español

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart'; // Importación necesaria para la localización en español e inglés
 import 'features/welcome/screens/welcome_screen.dart';
 import 'features/chat/providers/chat_thread_provider.dart';
 import 'features/chat/providers/chat_provider.dart';
@@ -8,7 +9,6 @@ import 'features/home/screens/home_screen.dart';
 import 'features/chat/screens/chattray_screen.dart';
 import 'features/profile/screens/profile_screen.dart';
 import 'features/calendar/screens/calendar_screen.dart';
-
 
 void main() {
   runApp(const MainApp());
@@ -21,6 +21,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        // Proveedores para la funcionalidad de chat
         Provider<ChatService>(
           create: (_) => ChatServiceImpl(),
         ),
@@ -36,7 +37,23 @@ class MainApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        initialRoute: '/',
+        // Establece el idioma de la aplicación en español.
+        locale: const Locale('es', 'ES'), // Aquí se configura el idioma predeterminado de la app como español (España).
+        
+        // Definición de los idiomas soportados por la aplicación.
+        supportedLocales: [
+          const Locale('en', 'US'), // Soporta inglés (Estados Unidos).
+          const Locale('es', 'ES'), // Soporta español (España).
+        ],
+        
+        // Delegados de localización necesarios para traducir los textos de la aplicación.
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate, // Traducción de componentes de Material Design (botones, diálogos, etc).
+          GlobalWidgetsLocalizations.delegate, // Traducción de componentes de widgets generales (texto, controles).
+        ],
+        
+        // Aquí se define la ruta inicial y las demás rutas de la aplicación.
+        initialRoute: '/', // La primera pantalla que se cargará será la WelcomeScreen.
         routes: {
           '/': (context) => const WelcomeScreen(),
           '/home': (context) => const HomeScreen(),
